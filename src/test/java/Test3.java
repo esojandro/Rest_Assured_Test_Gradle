031934113ab9d1bd05a2af836ce8a202778c8914
Alejandro Romero <esojandro@gmail.com>
2022-09-15 15:26:57 -0600

    Config changes

---
+++ src/test/java/Test3.java
@@ -0,0 +1,28 @@
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.junit.Before;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Test3 {
+
+    @Before
+    public void setup(){
+       RestAssured.baseURI = "https://reqres.in/";
+       RestAssured.basePath = "api/";
+       RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
+    @Test
+    public void getUserTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .get("users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2)); // Comprueba que el data.id es 2
+    }
+}
