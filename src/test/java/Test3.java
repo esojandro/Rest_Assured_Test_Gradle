e48f1f486ae89bd8703c98bc07277e8e935e0384
Alejandro Romero <esojandro@gmail.com>
2022-09-15 16:15:31 -0600

    Config changes and put, update test was added

--- src/test/java/Test3.java
+++ src/test/java/Test3.java
@@ -1,12 +1,15 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Test3 {
 
@@ -15,15 +18,55 @@ public class Test3 {
        RestAssured.baseURI = "https://reqres.in";
        RestAssured.basePath = "/api";
        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+       RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
     }
 
     @Test
     public void getUserTest(){
         given()
-                .contentType(ContentType.JSON)
                 .get("/users/2")
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .body("data.id", equalTo(2)); // Comprueba que el data.id es 2
+    }
+
+    @Test
+    public void deleteUserTest(){
+        given()
+                .delete("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+    @Test
+    public void pathUserTest(){ // El path es para actualizar solo un elemento del Json
+        String nameUpdated = given()
+                .when()
+                .body("{\n" +
+                        "\"name\": \"morpheus\",\n" +
+                        "\"job\": \"zion resident\"\n" +
+                        "}")
+                .patch("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().jsonPath().getString("name");
+
+        assertThat(nameUpdated, equalTo("morpheus"));
+    }
+
+    @Test
+    public void putUserTest(){
+        String jobUpdated = given()
+                .when()
+                .body("{\n" +
+                        "\"name\": \"morpheus\",\n" +
+                        "\"job\": \"zion resident\"\n" +
+                        "}")
+                .put("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().jsonPath().getString("job");
+
+        assertThat(jobUpdated, equalTo("zion resident"));
     }
 }
