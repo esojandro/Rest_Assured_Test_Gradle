2257cdc5b1af95d3be24f3ce349f7644062bf361
Alejandro Romero <esojandro@gmail.com>
2022-09-15 14:54:45 -0600

    Config changes

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,6 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
 
 public class Test1 {
     @Test
@@ -23,5 +24,40 @@ public class Test1 {
                 .asString();
 
         System.out.println(requestBody);
+    }
+
+    @Test
+    public void loginTestII(){
+
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "\"email\": \"eve.holt@reqres.in\",\n" +
+                        "\"password\": \"cityslicka\"\n" +
+                        "}")
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().all() //Para ver el token
+                .statusCode(200);
+    }
+
+    @Test
+    public void loginTestIII(){
+
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "\"email\": \"eve.holt@reqres.in\",\n" +
+                        "\"password\": \"cityslicka\"\n" +
+                        "}")
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().all() //Para ver el token
+                .statusCode(200)
+                .body("token", notNullValue()); // Comprueba que el token no sea nulo
     }
 }
---
+++ src/test/java/Test2.java
@@ -0,0 +1,9 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+
+public class Test2 {
+
+
+}
