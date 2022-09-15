b724141dad0c3733e06701144809a5af9375742d
Alejandro Romero <esojandro@gmail.com>
2022-09-15 15:00:45 -0600

    Config changes

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -5,5 +5,17 @@ import static org.hamcrest.Matchers.*;
 
 public class Test2 {
 
+    @Test
+    public void getUserTest(){
 
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().all() //Para ver el token
+                .statusCode(200)
+                .body("data.id", equalTo(2)); // Comprueba que el data.id es 2
+    }
 }
